a818754c46dbd9c88f07cdeb35f84dd46646c7ff
Samuel Witke <switke@signifyhealth.com>
2021-08-25 12:33:39 -0400

    fix(lint): lint check on test passing

--- apps/ctp.alumni.app/jest-preprocess.js
+++ apps/ctp.alumni.app/jest-preprocess.js
@@ -1,5 +1,6 @@
 const babelOptions = {
-    presets: ["babel-preset-gatsby"],
-  }
-  
-  module.exports = require("babel-jest").default.createTransformer(babelOptions)
+  presets: ['babel-preset-gatsby']
+};
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+module.exports = require('babel-jest').default.createTransformer(babelOptions);
