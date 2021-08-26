9a0b2e8eff74ee719e70cc47044190233a189e09
Samuel Witke <switke@signifyhealth.com>
2021-08-26 08:38:13 -0400

    fix(gatsby-ssr): adding analytics tag to gatsby ssr third-party package not working

---
+++ apps/ctp.alumni.app/gatsby-ssr.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      async
+      src="https://www.googleoptimize.com/optimize.js?id=OPT-NR3T8JQ"
+    ></script>,
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-EHPRWCNXFV"
+    ></script>,
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'UA-201299127-1');
+	gtag('config', 'G-EHPRWCNXFV');
+	`,
+      }}
+    ></script>,
+  ]);
+};
