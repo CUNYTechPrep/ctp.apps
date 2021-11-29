cd5d7545d2751444343dcdb4f6a258647d395ec2
Samuel Witke <switke@signifyhealth.com>
2021-11-29 16:28:54 -0500

    feat(ctp.alumni.newsletter): updates to newsletter

--- libs/ctp.alumni.newsletter/src/lib/ctp.alumni.newsletter.jsx
+++ libs/ctp.alumni.newsletter/src/lib/ctp.alumni.newsletter.jsx
@@ -1,6 +1,5 @@
 
-import { Actions,Mrkdwn, Blocks, Overflow, OverflowItem, Image, Header, Section, Context, Divider, Button} from 'jsx-slack'
-
+import { Actions,Mrkdwn, Field, Blocks, Overflow, OverflowItem, Image, Header, Section, Context, Divider, Button} from 'jsx-slack'
 
 export const CtpAlumniNewsletter = (props) => (
     <Blocks>
@@ -8,44 +7,70 @@ export const CtpAlumniNewsletter = (props) => (
       :newspaper:  CUNY Tech Alumni Newsletter  :newspaper:
     </Header>
     <Context>
-      *November 9, 2021*  |  CUNY Tech Announcements
+    <Mrkdwn >
+      `{new Date().toISOString()}`  |  # *CUNY Tech Announcements*
+      </Mrkdwn>
     </Context>
     <Divider />
     <Section>
       :calendar: |&nbsp;&nbsp;&nbsp;<b>UPCOMING EVENTS</b>&nbsp;&nbsp;| :calendar:
     </Section>
+      <Image src={"https://cunytechprep.nyc/img/banner-logo.png"} alt="Invite Image" />
     <Section>
       <b>
-      `12/14/2021` Demo Night
+      `12/14/2021` CUNY Tech Prep: Demo Night Fall 2021
       </b>
     <img src="https://cunytechprep.nyc/img/banner-logo.png" alt="yhatt" />
   </Section>
+  <Section>
+  Please join us for night of networking on Tuesday, December 14th from 5:30pm to 7:00pm
+    <Button url="https://cuny-tech-prep-demo-night-fall2021.eventbrite.com/?aff=AlumniNewsLetter" actionId="action" value="value" style="primary">
+     Click to RSVP 
+    </Button>
+  </Section>
   <Divider />
   <Section>
-      :calendar: |&nbsp;&nbsp;&nbsp;<b>PAST EVENTS</b>&nbsp;&nbsp;| :calendar:
+    <Mrkdwn >
+      :flashing_lights: *EVENT INFORMATION* :flashing_lights:
+    
+    </Mrkdwn>
+    <Field>
+      :five:
+      Break Out Rooms
+    </Field>
+    <Field>
+      :four::six: 
+      <br />
+      Teams
+    </Field>
+    <Field>
+      :three::zero:
+      Web Dev Projects
+    </Field>
+    <Field>
+      :one::seven: 
+      <br />
+      Data Science Projects
+    </Field>
+    Come on down and share the news with industry partners 
+      https://cuny-tech-prep-demo-night-fall2021.eventbrite.com/?aff=AlumniSlack
     </Section>
-    <Section>
-      <b>
-      `08/23-27/2021` *Welcome To CTP Hackathon* \n https://www.ctphackathon.com/
-      </b>
-    <img src="https://api.slack.com/img/blocks/bkb_template_images/notifications.png" alt="yhatt" />
-  </Section>
     <Divider />
     <Section>
       *Reminder* We are offering CTP alumni like you support with:
     </Section>
     <Actions>
     <Overflow actionId="overflow_menu">
-      <OverflowItem value="share">Technical interviews</OverflowItem>
-      <OverflowItem value="reply">Job searching</OverflowItem>
-      <OverflowItem url="https://example.com/">Open-source projects</OverflowItem>
+      <OverflowItem url="https://calendly.com/aem-ctp/45-meeting/">Technical interviews</OverflowItem>
+      <OverflowItem url="https://calendly.com/aem-ctp/45-meeting/">Job searching</OverflowItem>
+      <OverflowItem url="https://calendly.com/aem-ctp/45-meeting/">Open-source projects</OverflowItem>
     </Overflow>
   </Actions>
     <Divider />
     <Context>
     <Section>
     <Mrkdwn >
-      :pushpin: Do you have something to include in the newsletter? Please make a 
+      :pushpin: Do you have something to include in the newsletter? Please make a &nbsp;
       <a href="https://github.com/CUNYTechPrep/ctp.apps"> pull-request </a>
     </Mrkdwn>
   </Section>
