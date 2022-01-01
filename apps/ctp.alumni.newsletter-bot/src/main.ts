fde9aa58a893f4d6df2ae6fdf5a2d77e20d8248d
Samuel Witke <witkesam@gmail.com>
2022-01-01 18:53:22 -0500

    fix(ctp.alumni.newsletter): deploy

--- apps/ctp.alumni.newsletter-bot/src/main.ts
+++ apps/ctp.alumni.newsletter-bot/src/main.ts
@@ -1,6 +1,3 @@
-if(process.env.NODE_ENV !== "production") {
-  require('dotenv').config();
-}
 const { App } = require('@slack/bolt');
 import CtpAlumniNewsletter from '@cunytechprep/ctp.alumni.newsletter';
 /* 
