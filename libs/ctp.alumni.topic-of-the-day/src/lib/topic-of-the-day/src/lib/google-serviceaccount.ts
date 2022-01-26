722e0bbc4f0d05c9b077a9a8564601ae4db8c437
Samuel Witke <witkesam@gmail.com>
2022-01-26 08:22:45 -0500

    fix(ctp.alumnibot): try thi

--- libs/ctp.alumni.topic-of-the-day/src/lib/topic-of-the-day/src/lib/google-serviceaccount.ts
+++ libs/ctp.alumni.topic-of-the-day/src/lib/topic-of-the-day/src/lib/google-serviceaccount.ts
@@ -2,7 +2,7 @@ export default {
   type: process.env.GOOGLE_TYPE,
   project_id: process.env.GOOGLE_PROJECT_ID,
   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
   client_email:
     'topic-of-the-week-bot@effective-balm-337117.iam.gserviceaccount.com',
   client_id: process.env.GOOGLE_CLIENT_ID,
